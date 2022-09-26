a7593171c3dc5a57f7fd9c6c1cd4de47801cd41f
Shuwei Feng <shuwei@Shuweis-MBP.attlocal.net>
2022-09-26 07:38:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/flixsterpart1/BestSellerBook.kt
@@ -0,0 +1,37 @@
+package com.example.flixsterpart1
+
+import android.net.Uri
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class BestSellerBook {
+    @SerializedName("rank")
+    var rank = 0
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+    @JvmField
+    @SerializedName("overview")
+    var author: String? = null
+
+    //TODO bookImageUrl
+    @JvmField
+    @SerializedName("poster_path")
+    var bookImageUrl: String? = null
+
+    //TODO description
+    @SerializedName("description")
+    var description: String? = null
+
+    //TODO-STRETCH-GOALS amazonUrl
+    @SerializedName("amazon_product_url")
+    var amazonUrl: String? = null
+
+}
---
+++ app/src/main/java/com/example/flixsterpart1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.flixsterpart1
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flixsterpart1.R.id
+
+/**
+ * The MainActivity for the BestSellerList app.
+ * Launches a [BestSellerBooksFragment].
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val supportFragmentManager = supportFragmentManager
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(id.content, BestSellerBooksFragment(), null).commit()
+    }
+}
